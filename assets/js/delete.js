ed650daf1ac55ec69664665f9aed742782cb089f
gauri-plainsurf <46860151+gauri-plainsurf@users.noreply.github.com>
2019-07-10 18:35:39 +0530

    Add files via upload

---
+++ assets/js/delete.js
@@ -0,0 +1,13 @@
+
+        $(".remove").click(function () {
+        var id = $(this).parents("tr").attr("id");
+        if (confirm('Are you sure to remove this record ?'))
+        {
+                            window.location = "";  
+
+        }
+        });
+
+
+    
+
